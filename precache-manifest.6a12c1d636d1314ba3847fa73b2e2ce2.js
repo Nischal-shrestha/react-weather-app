22a60e537489317933b7f22ac4a46c7a89336bfa
Nischal Shrestha <developer.nish@gmail.com>
2018-10-07 00:53:09 +1000

    Updates

---
+++ precache-manifest.6a12c1d636d1314ba3847fa73b2e2ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "3f976c272ddbe4fab192",
+    "url": "/react-weather-app/static/js/main.3f976c27.chunk.js"
+  },
+  {
+    "revision": "83b3f9b8655729265301",
+    "url": "/react-weather-app/static/js/1.83b3f9b8.chunk.js"
+  },
+  {
+    "revision": "3f976c272ddbe4fab192",
+    "url": "/react-weather-app/static/css/main.17d309e3.chunk.css"
+  },
+  {
+    "revision": "6e44c9cb9112c4769db6813df8e17c1e",
+    "url": "/react-weather-app/index.html"
+  }
+];
